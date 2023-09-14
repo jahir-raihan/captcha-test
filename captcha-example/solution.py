15db6c5f7d333ed42bb96f829df877a83549f83e
jahir-raihan <raihan.joy10@gmail.com>
2023-09-15 03:20:26 +0600

    updated pacakge

--- captcha-example/solution.py
+++ captcha-example/solution.py
@@ -2,7 +2,7 @@ from undetected_chromedriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from twocaptcha import TwoCaptcha
+from twocaptcha.solver import TwoCaptcha
 from time import sleep
 
 url = 'https://www.google.com/recaptcha/api2/demo'
@@ -13,10 +13,10 @@ WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'recaptc
 site_key = browser.find_element(By.ID, 'recaptcha-demo').get_attribute('data-sitekey')
 
 solver = TwoCaptcha('f03499fd11615e8347f6aa29254be92d')  # -> API Key
-result = solver.solve_captcha(site_key, url)    # Solving Recaptcha
+result = solver.recaptcha(site_key, url)    # Solving Recaptcha
 
 print(result)
-browser.execute_script(f'document.getElementById("g-recaptcha-response").value = "{result}"')   # Inserting result code
+browser.execute_script(f'document.getElementById("g-recaptcha-response").value = "{result["code"]}"')   # Inserting result code
 
 browser.find_element(By.XPATH, '//*[@id="recaptcha-demo-submit"]').click()  # Submitting form
 sleep(50)
