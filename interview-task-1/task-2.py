ba7bda5597d83a7cbd9e84bdfce925db61653c3e
jahir-raihan <raihan.joy10@gmail.com>
2023-10-17 00:06:11 +0600

    Update script for google places

--- interview-task-1/task-2.py
+++ interview-task-1/task-2.py
@@ -1,18 +1,2 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
-
-
-def main():
-    chrome_options = webdriver.ChromeOptions()
-    ip = 'yjixarcq:2y4mfkttnrmi@172.245.7.2:5055'
-
-    chrome_options.add_argument(f'--proxy-server={ip}')
-
-    url = 'https://whatismyipaddress.com'
-    browser = webdriver.Chrome(options=chrome_options)
-    browser.get(url)
-
-    sleep(10)
-
-main()
+f = open('hello.txt', 'a')
+f.writelines("hello outta here dude 2")
